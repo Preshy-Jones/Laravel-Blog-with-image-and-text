4066c201a7b49041b5639eb82bdf29898782ff71
Preshy-Jones <adedibuprecious@gmail.com>
2020-06-28 02:23:13 +0100

    initial commit

--- app/Http/Controllers/Postscontroller.php
+++ app/Http/Controllers/Postscontroller.php
@@ -64,7 +64,7 @@ class Postscontroller extends Controller
 
         $fileNameToStore = $filename.'_'.time().'.'.$extension;
 
-        $path = $request->file('cover_image')->storeAs('public/cover_images',$fileNameToStore);
+        $path = $request->file('cover_image')->storeAs('/cover_images',$fileNameToStore);
     } else{
         $fileNameToStore = 'noimage.jpg';
     }
@@ -169,7 +169,7 @@ class Postscontroller extends Controller
         }
         $post->delete();
 
-        return redirect('/posts')->with('success', 'Post Post Removed');
+        return redirect('/posts')->with('success', 'Post Po st Removed');
    
     }
 }
